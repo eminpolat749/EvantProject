6de0072fcb5691ca64af246303bb6310f42dc170
Emin Polat <51139204+eminpolat749@users.noreply.github.com>
2025-07-24 18:32:10 +0300

    before the esp32 test

---
+++ Esp32TestProject/src/main/java/com/eminpolat/esp32testproject/Esp32TestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.eminpolat.esp32testproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esp32TestProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Esp32TestProjectApplication.class, args);
+    }
+
+}
---
+++ Esp32TestProject/src/test/java/com/eminpolat/esp32testproject/Esp32TestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eminpolat.esp32testproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Esp32TestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
